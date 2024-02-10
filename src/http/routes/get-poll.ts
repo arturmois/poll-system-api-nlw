e559526279096ac3feca0c3f82493b84297d9910
Artur Moisés <arturmoiscontato@gmail.com>
2024-02-10 12:56:59 -0300

    feat: create get poll route

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod'
+import { prisma } from '../../lib/prisma'
+import { FastifyInstance } from 'fastify'
+
+export async function getPoll(app: FastifyInstance) {
+  app.get('/polls/:pollId', async (request, reply) => {
+    const getPollParams = z.object({
+      pollId: z.string().uuid()
+    })
+
+    const { pollId: id } = getPollParams.parse(request.params)
+
+    const poll = await prisma.poll.findUnique({
+      where: {
+        id
+      },
+      include: {
+        options: {
+          select: {
+            id: true,
+            title: true
+          }
+        }
+      }
+    })
+
+    return reply.send(poll)
+  })
+}
